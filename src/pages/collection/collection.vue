653ae274097ca8fcf12eadd1ef845555905dec77
Andius76 <3182541950@qq.com>
2025-04-20 21:26:55 +0800

    修改article-card的margin-top

--- src/pages/collection/collection.vue
+++ src/pages/collection/collection.vue
@@ -1,13 +1,30 @@
 <template>
 	<view>
-		
+		<button @click="login">跳转到登录</button>
+		<button @click="index">跳转到首页</button>
 	</view>
 </template>
 
 <script setup>
 	
+const login = () => {
+	uni.navigateTo({
+		url: '/pages/login/login'
+	});
+}
+const index = () => {
+	uni.navigateTo({
+		url: '/pages/index/index'
+	});
+}
 </script>
 
-<style>
-	       
+<style lang="scss">
+	button {
+		width: 100%;
+		height: 40px;
+		border: 1px solid #ccc;
+		border-radius: 5px;
+		margin-bottom: 10px;
+	}
 </style>
