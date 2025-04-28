e90fb55fde500ace0a75f2418066df01496e45ee
Andius76 <3182541950@qq.com>
2025-04-28 11:20:01 +0800

    修改request.js App的开发服务器地址

--- src/utils/request.js
+++ src/utils/request.js
@@ -21,7 +21,7 @@ function getBaseUrl() {
   // 获取当前运行的平台
   // #ifdef APP-PLUS
   // APP端不能使用localhost/127.0.0.1，需要使用本机IP地址
-  return 'http://10.9.69.193:8080'; // 请替换为开发服务器的IP地址
+  return 'http://10.9.248.114:8080'; // 请替换为开发服务器的IP地址
   // #endif
   
   // #ifdef H5 || MP-WEIXIN
